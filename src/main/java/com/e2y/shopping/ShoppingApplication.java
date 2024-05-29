17ff2c837714ac4821ce8ef77a71a3c7b2d9fe53
Ghizlane EL IDRISSI <ghizlaneelidrissi@Ghizlanes-MBP.lan>
2024-05-29 15:40:39 +0200

    first commit

---
+++ src/main/java/com/e2y/shopping/ShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.e2y.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/e2y/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2y.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
